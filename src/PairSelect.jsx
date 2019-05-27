fc22f78105b20c161b833120423d0467a0a7bade
Mateusz Kaluza <mateo.kaluza@gmail.com>
2019-05-27 23:51:08 +0200

    Separate select component

--- src/PairSelect.jsx
+++ src/PairSelect.jsx
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import { Input } from 'reactstrap';
+import React, {Component} from 'react';
+import {Input, FormFeedback, Spinner} from 'reactstrap';
 
 class PairSelect extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '', pairs: [] };
+        this.state = {value: '', pairs: [], isValid: true, isLoading: true};
         this.toggle = this.toggle.bind(this);
         this.handlePairSelect = this.handlePairSelect.bind(this);
     }
 
     render() {
+        let select = (
+            <>
+                <Input invalid={!this.state.isValid}
+                       disabled={!this.state.isValid}
+                       type="select" name="pair"
+                       value={this.state.value}
+                       onChange={this.handlePairSelect}>
+                    {
+                        this.state.pairs.map(pair => <option key={pair}>{pair}</option>)
+                    }
+                </Input>
+                <FormFeedback valid={this.state.isValid}>Oops! Something went wrong :(</FormFeedback>
+            </>);
+
+        let spinner = <Spinner color="light"/>;
+
         return (
-            <Input type="select" name="pair" value={this.state.value} onChange={this.handlePairSelect} >
-                {
-                    this.state.pairs.map(pair => <option key={pair}>{pair}</option>)
-                }
-            </Input>
+            this.state.isLoading ? spinner : select
         );
     }
 
@@ -26,24 +38,21 @@ class PairSelect extends Component {
     }
 
     handlePairSelect(event) {
-        this.setState({ value: event.target.value });
+        this.setState({value: event.target.value});
     }
 
     componentDidMount() {
-        fetch('https://api.bitfinex.com/v1/symbols_details', {
-            mode: 'no-cors', 
-            // method: 'GET',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-            }
-        })
-        // .then(response => response.json())
-        // .then((data) => {
-        //     const pairs = data.map(element => element.pair);
-        //     this.setState({pairs})
-        // })
+        fetch('https://api.bitfinex.com/v1/symbols_details')
+            .then(response => response.json())
+            .then((data) => {
+                const pairs = data.map(element => element.pair);
+                this.setState({pairs, isLoading: false})
+            })
+            .catch(() => {
+                this.setState({isValid: false, isLoading: false})
+            });
     }
-};
+}
 
 
 export default PairSelect;
